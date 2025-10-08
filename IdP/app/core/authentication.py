0fd8cee1cf2f0c36c71b0ef17cdc65946e8a531d
diaco <shilahassanabadi@gmail.com>
2025-10-08 09:43:23 +0330

    password - test

--- IdP/app/core/authentication.py
+++ IdP/app/core/authentication.py
@@ -54,14 +54,13 @@ def load_keys():
 
     return private_key, public_key
 
-
 private_key, public_key = load_keys()
 
-
+#--------------------------------------- Hash Password
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-
+#--------------------------------------- Verify password
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
