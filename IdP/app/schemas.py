4a01d496196e3f8deb9a6d13d2861bdd6b2f8bc1
diaco <shilahassanabadi@gmail.com>
2025-10-13 16:20:53 +0330

    schema new endpoint

--- IdP/app/schemas.py
+++ IdP/app/schemas.py
@@ -1,9 +1,11 @@
 # schemas.py - Pydantic models
-from pydantic import BaseModel, EmailStr
-from datetime import datetime
-from typing import Optional, List
+from pydantic import BaseModel, EmailStr,Field,validator
+from typing import Optional, List, Literal
 from enum import Enum
 from uuid import UUID
+from datetime import datetime
+from app.core.config import settings
+
 
 class UserRoleSchema(str, Enum):
     ADMIN = "admin"
@@ -20,6 +22,7 @@ class UserRegister(BaseModel):
     username: str
     email: EmailStr
     password: str
+    mobile: str
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     role: Optional[UserRoleSchema] = UserRoleSchema.USER
@@ -28,6 +31,7 @@ class UserResponse(BaseModel):
     id: UUID
     username: str
     email: str
+    mobile: str
     first_name: Optional[str]
     last_name: Optional[str]
     role: UserRoleSchema
@@ -35,25 +39,12 @@ class UserResponse(BaseModel):
     created_at: datetime
     last_login: Optional[datetime]
 
-class TokenRequest(BaseModel):
-    grant_type: str
-    code: Optional[str] = None
-    refresh_token: Optional[str] = None
-    client_id: str
-    client_secret: str
-    redirect_uri: Optional[str] = None
-
-class TokenResponse(BaseModel):
-    access_token: str
-    refresh_token: str
-    expires_in: int
-    token_type: str = "Bearer"
-    scope: str
 
 class UserInfo(BaseModel):
     sub: str
     username: str
     email: str
+    mobile: Optional[str] = None
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     role: str
@@ -74,9 +65,17 @@ class ClientResponse(BaseModel):
 class LoginRequest(BaseModel):
     username: str
     password: str
-    client_id: Optional[str] = None
+    client_id: Optional[str] = settings.OAUTH_CLIENT_ID
     scope: Optional[str] = "openid profile email"
     state: Optional[str] = None
+
+class LoginPasswordRequest(BaseModel):
+    email_or_mobile: str
+    password: str
+    client_id: Optional[str] = settings.OAUTH_CLIENT_ID
+    scope: Optional[str] = "openid profile email"
+    state: Optional[str] = None
+
 
 class LoginResponse(BaseModel):
     access_token: str
@@ -98,4 +97,71 @@ class AuthorizeResponse(BaseModel):
     client_name: Optional[str] = None
     requested_scopes: list
     state: Optional[str] = None
+    message: str
+
+
+
+class SendCodeRequest(BaseModel):
+    email_or_mobile: str
+    mode: Literal["register", "login", "forgot_password"]
+
+class LoginOtpRequest(BaseModel):
+    email_or_mobile: str
+    code: str
+    state: str | None = None
+    scope: str = "openid profile"
+
+class LoginOtpResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    expires_in: int
+    token_type: str
+    user: dict
+    scope: str | None
+    state: str
+    redirect_url: str
+
+
+class RegisterRequest(BaseModel):
+    email_or_mobile: Optional[str] = ""
+    email: str
+    mobile: str
+    code: str
+    first_name: Optional[str] = ""
+    last_name: Optional[str] = ""
+    password: str
+    re_password: str
+
+
+class RegisterResponse(BaseModel):
+    message: str
+
+
+
+class ChangePasswordRequest(BaseModel):
+    current_password: str = Field(..., min_length=6)
+    password: str = Field(..., min_length=8)
+    re_password: str = Field(..., min_length=8)
+    @validator("re_password")
+    def passwords_match(cls, v, values):
+        if "password" in values and v != values["password"]:
+            raise ValueError("Passwords do not match")
+        return v
+
+class ChangePasswordResponse(BaseModel):
+    message: str
+
+class ResetPasswordRequest(BaseModel):
+    email_or_mobile: str
+    code: str
+    password: str = Field(..., min_length=8)
+    re_password: str = Field(..., min_length=8)
+
+    @validator("re_password")
+    def passwords_match(cls, v, values):
+        if "password" in values and v != values["password"]:
+            raise ValueError("Passwords do not match")
+        return v
+
+class ResetPasswordResponse(BaseModel):
     message: str
